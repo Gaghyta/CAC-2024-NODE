64a995fa07a439fb41c8677d8c2b93d5abb49653
Carla Antonini <carla.antonini@hotmail.com>
2024-07-09 20:48:31 -0300

    CA modif en ruta y archivo index

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+const { createPool } = require('mysql2/promise');
 //const fetch = require('node-fetch');
 require('dotenv').config(); // Para cargar las variables de entorno desde .env
 
@@ -11,26 +12,51 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
 
+// ************** PRUEBAS CONEXIONES BD **************
 // Configuración de la conexión a la base de datos
-const db = mysql.createConnection({
+/* const db = mysql.createConnection({
     host: process.env.MYSQL_ADDON_HOST,
     user: process.env.MYSQL_ADDON_USER,
     password: process.env.MYSQL_ADDON_PASSWORD,
     database: process.env.MYSQL_ADDON_DB,
-});
+}); */
+
+// Configuración de la conexión a la base de datos
+/* const dbConfig = {
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+};
+ */
+
+
+const pool = createPool({
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    connectionLimit: 10,
+
+ })
+
+
 
 // Conectar a la base de datos
-db.connect((err) => {
+/* db.connect((err) => {
     if (err) {
         console.error('Error conectando a la base de datos:', err);
         return;
     } console.log('Conectado a la base de datos MySQL');
 });
-
+ */
 //  archivos estáticos
+
+// ************** PRUEBAS APP GET VIEWS **************
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para el home
@@ -75,7 +101,7 @@ app.get('/contacto.html', (req, res) => {
 }); 
  */
 
-app.get('/usuarios.html', (req, res) => {
+/* app.get('/usuarios.html', (req, res) => {
     const sql = 'SELECT * FROM usuarios'; // Ajusta según el nombre real de tu tabla de usuarios
     db.query(sql, (err, results) => {
         if (err) {
@@ -86,10 +112,89 @@ app.get('/usuarios.html', (req, res) => {
         res.json(results);
     });
 });
+ */
+/* app.get('/usuarios', async (req, res) => {
+    const sql = 'SELECT * FROM usuarios'; // Ajusta según el nombre real de tu tabla de usuarios
+
+    try {
+        // Crear una conexión a la base de datos
+        const connection = await mysql.createConnection(dbConfig);
+        console.log('Conexión a la base de datos establecida');
+        // Ejecutar la consulta
+        const [results, fields] = await connection.execute(sql);
+        console.log('Consulta ejecutada, resultados:', results);
+        // Cerrar la conexión
+        await connection.end();
+        console.log('Conexión a la base de datos cerrada');
+
+        res.render('usuarios', { usuarios: results });
+        // Enviar los resultados como JSON
+        //res.json(results);
+    } catch (err) {
+        console.error('Error al obtener usuarios:', err);
+        res.status(500).json({ error: 'Error en la base de datos' });
+    }
+}); */
 
 //app.use(express.static(path.join(__dirname, 'views')));
+
+
+// ************** PRUEBAS APP GET VIEW USUARIOS DE LA BD 1 **************
+/* app.get('/usuarios', async (req, res) => {
+    try {
+        const usuarios = await obtenerUsuariosDeLaBaseDeDatos();
+        res.render('usuarios', { usuarios: usuarios });
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).json({ error: 'Error en la base de datos' });
+    }
+});
+
+async function obtenerUsuariosDeLaBaseDeDatos() {
+    const connection = await mysql.createConnection(dbConfig);
+    console.log('Conexión a la base de datos establecida');
+    const [results, fields] = await connection.execute('SELECT * FROM usuarios');
+    console.log('Consulta ejecutada, resultados:', results);
+    await connection.end();
+    console.log('Conexión a la base de datos cerrada');
+    return results;
+} */
+
+// ************** PRUEBAS APP GET VIEW USUARIOS DE LA BD 2 **************
+async function obtenerUsuarios() {
+    const connection = await pool.getConnection();
+    try {
+        const [rows, fields] = await connection.query('SELECT * FROM usuarios');
+        return rows;
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        throw error;
+    } finally {
+        connection.release(); // Liberar la conexión de vuelta a la pool
+    }
+}
+app.get('/usuarios.html', async (req, res) => {
+    try {
+        const usuarios = await obtenerUsuarios();
+        res.render('usuarios', { usuarios: usuarios });
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        res.status(500).json({ error: 'Error en la base de datos' });
+    }
+});
+
+
+// Uso de la función para obtener usuarios
+/* obtenerUsuarios().then((usuarios) => {
+    console.log('Usuarios:', usuarios);
+}).catch((error) => {
+    console.error('Error en la consulta:', error);
+});
+ */
+
 
 // Iniciar el servidor
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
